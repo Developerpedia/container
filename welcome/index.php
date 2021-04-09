171de04dcad15fccc5f91b158729d74d0bc969d2
Developerpedia <68114246+Developerpedia@users.noreply.github.com>
2021-04-09 17:21:18 +0530

    okay

---
+++ welcome/index.php
@@ -0,0 +1,9 @@
+<?php
+ echo ' 
+ <ul>
+ <li><a href="#gallery">Gallery</a></li>
+ <li><a href="#brochure">Brochure</a></li>
+ <li><a href="#blog">Blog</a></li>
+ <li><a href="#home">Home</a></li>
+ </ul>'  
+?>                 
---
+++ welcome/welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+<link rel="stylesheet" type="text/css" href="resources/CSS/style.css">
+ </head>
+ <body>
+ <div class="menu";?>
+ <?php include 'index.php';?>    
+ <h1>Welcome to our Page</h1>
+ <p>"We are delighted to have you among us"</p>     
+ </div>
+ </body>
+</html>
